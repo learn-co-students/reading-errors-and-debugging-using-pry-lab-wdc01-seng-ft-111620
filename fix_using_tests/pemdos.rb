d679a120ccd747745cfd4996e232636da05153fc
John Fuquene <jfuquene@email.wm.edu>
2020-11-06 23:24:06 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if string[0] === "s"
+   new_string = "s" * 10 + string 
+   return new_string 
   else
     string
   end
